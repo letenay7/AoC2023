8418eed6b4c89c53a9b8658bebca8795939f6e70
letenay7 <99814299+letenay7@users.noreply.github.com>
2023-12-01 15:08:50 +0100

    Day 1 (ugly solution)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            val digits = line.filter { it.isDigit() }
+            val calibration =  "" + digits.first() + digits.last()
+            sum += calibration.toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val digits = setOf(
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        for (line in input) {
+            val numbers = mutableListOf<String>()
+            for(( i, ch )in line.withIndex()) {
+                if (ch.isDigit()) {
+                    numbers.add(ch.toString())
+                }
+                for( (index, value) in digits.withIndex() ) {
+                    val sub = line.substring(i)
+                    if (sub.startsWith(value)) {
+                        numbers.add((index + 1).toString())
+                    }
+                }
+            }
+            val num = "" + numbers.first() + numbers.last()
+            sum += num.toInt()
+
+
+        }
+        return sum
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+
+
 
     val input = readInput("Day01")
+
     part1(input).println()
     part2(input).println()
 }
